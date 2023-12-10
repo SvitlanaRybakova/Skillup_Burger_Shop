adee64d6874a83083611af4627cfd3198a7f6e00
Svitlana Rybakova <rsv0011@gmail.com>
2023-12-10 15:59:03 +0100

    feat: implement the Contact Page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,1 +1,141 @@
-// Write all the code here
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import burger from '../../assets/burger2.png';
+import { useNavigate } from 'react-router-dom';
+
+const Contact = () => {
+  const navigate = useNavigate();
+
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSendClick = () => {
+    const allFieldsFilled = Object.values(userData).every(
+      (field) => field.trim() !== ''
+    );
+
+    if (!allFieldsFilled) {
+      setError(true);
+      return;
+    }
+
+    setSuccess(true);
+  };
+
+  const dialogStyle = {
+    position: 'fixed',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: 1001,
+    backgroundColor: '#fff',
+    padding: '20px',
+    borderRadius: '5px',
+    boxShadow: '0 0 10px rgba(0, 0, 0, 0.2)',
+    textAlign: 'center',
+  };
+
+  const closeButtonStyle = {
+    marginTop: '20px',
+    padding: '10px 20px',
+    backgroundColor: 'rgb(156, 0, 60)',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '15px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+  };
+
+  return (
+    <section className='contact'>
+      <motion.form
+        initial={{ x: '-100vw', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.2 }}
+      >
+        <h2>Contact Us</h2>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          onChange={handleInputChange}
+        />
+        <input
+          type='text'
+          name='email'
+          placeholder='Email'
+          onChange={handleInputChange}
+        />
+        <textarea
+          name='message'
+          placeholder='Message...'
+          cols='30'
+          rows='10'
+          onChange={handleInputChange}
+        ></textarea>
+        <button type='button' onClick={handleSendClick}>
+          Send
+        </button>
+
+        {error && (
+          <div style={dialogStyle}>
+            Please complete all fields before sending.
+            <br />
+            <button
+              type='button'
+              onClick={() => setError(false)}
+              style={closeButtonStyle}
+            >
+              Close
+            </button>
+          </div>
+        )}
+
+        {success && (
+          <div style={dialogStyle}>
+            Thank you for contacting us! We will respond to you as soon as
+            possible.
+            <br />
+            <button
+              type='button'
+              onClick={() => {
+                setSuccess(false);
+                navigate('/');
+              }}
+              style={closeButtonStyle}
+            >
+              Close
+            </button>
+          </div>
+        )}
+      </motion.form>
+      <motion.div
+        className='formBorder'
+        initial={{ x: '100vw', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ delay: 0.2 }}
+      >
+        <motion.div
+          initial={{ y: '-100vh', x: '50%', opacity: 0 }}
+          animate={{ x: '50%', y: '-50%', opacity: 1 }}
+          transition={{ delay: 1 }}
+        >
+          <img src={burger} alt='Burger' />
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,1 +1,187 @@
-// Write all the code here
+//    
+import React, { useState } from "react";
+import {Link} from 'react-router-dom'
+import {
+  MDBContainer, MDBTabs, MDBTabsItem, MDBTabsLink, MDBTabsContent, MDBTabsPane, MDBBtn, MDBIcon, MDBInput, MDBCheckbox,
+} from "mdb-react-ui-kit";
+
+function Login() {
+  const [justifyActive, setJustifyActive] = useState("tab1");
+
+  const handleJustifyClick = (value) => {
+    if (value === justifyActive) {
+      return;
+    }
+    setJustifyActive(value);
+  };
+
+  return (
+    <MDBContainer className='p-3 my-5 d-flex flex-column w-50'>
+      <MDBTabs
+        pills
+        justify
+        className='mb-3 d-flex flex-row justify-content-between'
+      >
+        <MDBTabsItem>
+          <MDBTabsLink
+            onClick={() => handleJustifyClick('tab1')}
+            active={justifyActive === 'tab1'}
+          >
+            Login
+          </MDBTabsLink>
+        </MDBTabsItem>
+        <MDBTabsItem>
+          <MDBTabsLink
+            onClick={() => handleJustifyClick('tab2')}
+            active={justifyActive === 'tab2'}
+          >
+            Register
+          </MDBTabsLink>
+        </MDBTabsItem>
+      </MDBTabs>
+      <MDBTabsContent>
+        <MDBTabsPane show={justifyActive === 'tab1'}>
+          <div className='text-center mb-3'>
+            <p>Sign in with:</p>
+            <div
+              className='d-flex justify-content-between mx-auto'
+              style={{ width: '40%' }}
+            >
+              <MDBBtn
+                tag='a'
+                color='none'
+                className='m-1'
+                style={{ color: '#1266f1' }}
+              >
+                <MDBIcon fab icon='facebook-f' size='sm' />
+              </MDBBtn>
+              <MDBBtn
+                tag='a'
+                color='none'
+                className='m-1'
+                style={{ color: '#1266f1' }}
+              >
+                <MDBIcon fab icon='twitter' size='sm' />
+              </MDBBtn>
+              <MDBBtn
+                tag='a'
+                color='none'
+                className='m-1'
+                style={{ color: '#1266f1' }}
+              >
+                <MDBIcon fab icon='google' size='sm' />
+              </MDBBtn>
+              <MDBBtn
+                tag='a'
+                color='none'
+                className='m-1'
+                style={{ color: '#1266f1' }}
+              >
+                <MDBIcon fab icon='github' size='sm' />
+              </MDBBtn>
+            </div>
+            <p className='text-center mt-3'>or:</p>
+          </div>
+          <MDBInput
+            wrapperClass='mb-4'
+            label='Email address'
+            id='form1'
+            type='email'
+          />
+          <MDBInput
+            wrapperClass='mb-4'
+            label='Password'
+            id='form2'
+            type='password'
+          />
+          <div className='d-flex justify-content-between mx-4 mb-4'>
+            <MDBCheckbox
+              name='flexCheck'
+              value=''
+              id='flexCheckDefault'
+              label='Forgot password?'
+            />
+            <Link to={'/'}>Forgot password</Link>
+          </div>
+          <MDBBtn className='mb-4 w-100'>Remember me</MDBBtn>
+          <p className='text-center'>
+            Not a member? <a href='#!'>Register</a>
+          </p>
+        </MDBTabsPane>
+        {/* TAB 2 */}
+        <MDBTabsPane show={justifyActive === 'tab2'}>
+          <div className='text-center mb-3'>
+            <p>Sign in with:</p>
+            <div
+              className='d-flex justify-content-between mx-auto'
+              style={{ width: '40%' }}
+            >
+              <MDBBtn
+                tag='a'
+                color='none'
+                className='m-1'
+                style={{ color: '#1266f1' }}
+              >
+                <MDBIcon fab icon='facebook-f' size='sm' />
+              </MDBBtn>
+              <MDBBtn
+                tag='a'
+                color='none'
+                className='m-1'
+                style={{ color: '#1266f1' }}
+              >
+                <MDBIcon fab icon='twitter' size='sm' />
+              </MDBBtn>
+              <MDBBtn
+                tag='a'
+                color='none'
+                className='m-1'
+                style={{ color: '#1266f1' }}
+              >
+                <MDBIcon fab icon='google' size='sm' />
+              </MDBBtn>
+              <MDBBtn
+                tag='a'
+                color='none'
+                className='m-1'
+                style={{ color: '#1266f1' }}
+              >
+                <MDBIcon fab icon='github' size='sm' />
+              </MDBBtn>
+            </div>
+            <p className='text-center mt-3'>or:</p>
+          </div>
+          <MDBInput
+            wrapperClass='mb-4'
+            label='First Name'
+            id='form3'
+            type='text'
+          />
+          <MDBInput
+            wrapperClass='mb-4'
+            label='Last Name'
+            id='form4'
+            type='text'
+          />
+          <MDBInput wrapperClass='mb-4' label='Email' id='form5' type='email' />
+          <MDBInput
+            wrapperClass='mb-4'
+            label='Password'
+            id='form6'
+            type='password'
+          />
+          <div className='d-flex justify-content-center mb-4'>
+            <MDBCheckbox
+              name='flexCheck'
+              id='flexCheckDefault'
+              label='I have read and accept the conditions'
+            />
+          </div>
+          <MDBBtn className='mb-4 w-100'>Register</MDBBtn>
+        </MDBTabsPane>
+      </MDBTabsContent>
+    </MDBContainer>
+  );
+}
+
+export default Login;
