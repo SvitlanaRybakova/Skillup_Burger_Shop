00bbf4554f94f16fe0dbffab7a34f1bd4eaa6ba8
Svitlana Rybakova <rsv0011@gmail.com>
2023-12-10 15:10:31 +0100

    feat: create the Header && Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,1 +1,29 @@
-// Write all the code here
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiFillInstagram, AiFillYoutube, AiFillGithub } from 'react-icons/ai';
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2>Burger Shop</h2>
+        <p>We are trying to give you the best taste possible</p>
+        <br />
+        <em>We give attention to genuine feedback</em>
+        <strong>All right received @burgershop</strong>
+      </div>
+      <aside>
+        <h4>Follow Us</h4>
+        <Link to={'https://youtube.com'}>
+          <AiFillYoutube />
+        </Link>
+        <Link to={'https://instagram.com'}>
+          <AiFillInstagram />
+        </Link>
+        <Link to={'https://github.com'}>
+          <AiFillGithub />
+        </Link>
+      </aside>
+    </footer>
+  );
+};
+export default Footer;
